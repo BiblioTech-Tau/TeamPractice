ad0df39c16fcf2b889643b14afcd199c6fbb9bcf
BiblioTech-Tau <bibliotechtau@gmail.com>
2021-08-27 15:56:43 -0400

    Create Tau.py

---
+++ Tau.py
@@ -0,0 +1,10 @@
+print ("your mom")
+
+answer = input("Are you sad about what I said? Y/N")
+
+
+if answer =="Y": 
+    print ("Got em")
+
+if answer =="N":
+    print ("You are a emotionless child...")
